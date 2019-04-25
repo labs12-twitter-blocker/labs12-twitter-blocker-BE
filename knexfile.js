b268d8d3729bee9686c84911c1e74df73a0d9f89
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-25 15:19:58 -0700

    Added migrations for the twitter_users and app_users tables

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'twitter',
+      user:     'twitdev',
+      password: 'qxZVf5d9'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, '0.0.0.0') 
+app.listen(port, () => console.log(`\n** server up on port ${port} **\n`)) 
