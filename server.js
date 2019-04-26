79491dccb29ed85b2bd2b859fbdde6463387b168
Erin Meredith <ekmeredith@att.net>
2019-04-26 07:49:22 -0400

    seeded database with mock data; added cors and simple get endpoint to server file

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors());
+
+
+const db = require('./data/db')
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    db('twitter_users')
+    .then(response => {
+        res.status(200).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'The users information could not be retrieved.'})
+    })
 })
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`\n** server up on port ${port} **\n`)) 
