83b6011d23b2ef8bacd6b4f2090a1be3b76f87f6
Erin Meredith <ekmeredith@att.net>
2019-04-26 10:47:14 -0400

    moved endpoints to users file in api directory

--- server.js
+++ server.js
@@ -5,35 +5,8 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors());
 
-
-const db = require('./data/db')
-
-app.get('/', (req, res) => {
-    db('twitter_users')
-    .then(response => {
-        res.status(200).json(response)
-    })
-    .catch(err => {
-        res.status(500).json({error: 'The users information could not be retrieved.'})
-    })
-})
-
-app.get('/:id', (req, res) => {
-    const id = req.params.id;
-    if(!id){
-        res.status(404).json({error: 'The user with the specified ID does not exist.'})
-        return;
-    }
-    db('twitter_users as t')
-    .where('t.twitter_users_id', id)
-    .then(response => {
-        res.status(200).json(response)
-    })
-    .catch(err => {
-        res.status(500).json({error: 'The user information could not be retrieved.'})
-    })
-})
-
+const userRoutes = require('./api/users');
+app.use('/', userRoutes);
 
 const port = process.env.PORT || 5000;
 
