9b3a0de84b29937cdf4dc6c45caeb1ca1eec86e6
Daniel <danielstarling4@gmail.com>
2019-04-26 14:59:36 -0700

    fixed the auth route. It should now work

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,20 +1,15 @@
 // AUTH ROUTE FOR OAUTH AUTHORIZATION
+require('dotenv').config();
 const router = require('express').Router();
 const passport = require('passport');
 const Strategy = require('passport-twitter').Strategy;
 
-require('dotenv').config();
-
-const FE_URL = process.env.FRONT_END_URL || 'http://localhost:3000';
-const CB_URL =
-  process.env.CALLBACK_URL || 'http://localhost:5000/auth/twitter/callback';
-const test = 'https://twitter-blocker.netlify.com/';
 passport.use(
   new Strategy(
     {
       consumerKey: process.env.TWITTER_CONSUMER_KEY,
       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      callbackURL: CB_URL
+      callbackURL: process.env.CALLBACK_URL
     },
     function(token, tokenSecret, profile, callback) {
       return callback(null, profile);
@@ -35,10 +30,10 @@ router.get('/twitter/login', passport.authenticate('twitter'));
 router.get(
   '/twitter/callback',
   passport.authenticate('twitter', {
-    failureRedirect: FE_URL
+    failureRedirect: process.env.FRONT_END_URL
   }),
   function(req, res) {
-    res.redirect(test);
+    res.redirect(process.env.FRONT_END_URL);
   }
 );
 module.exports = router;
