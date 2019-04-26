091143ef643c3564a30307cfb6e64f0fab3fa6cd
Daniel <danielstarling4@gmail.com>
2019-04-26 13:52:03 -0700

    added better handeling for local oauth handling

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,14 +5,16 @@ const Strategy = require('passport-twitter').Strategy;
 
 require('dotenv').config();
 
+const FE_URL = process.env.FRONT_END_URL || 'http://localhost:3000';
+const CB_URL =
+  process.env.CALLBACK_URL || 'http://localhost:5000/auth/twitter/callback';
+const test = 'https://twitter-blocker.netlify.com/';
 passport.use(
   new Strategy(
     {
       consumerKey: process.env.TWITTER_CONSUMER_KEY,
       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      callbackURL:
-        process.env.CALLBACK_URL ||
-        'http://localhost:5000/auth/twitter/callback'
+      callbackURL: CB_URL
     },
     function(token, tokenSecret, profile, callback) {
       return callback(null, profile);
@@ -33,10 +35,10 @@ router.get('/twitter/login', passport.authenticate('twitter'));
 router.get(
   '/twitter/callback',
   passport.authenticate('twitter', {
-    failureRedirect: 'https://twitter-block.herokuapp.com/auth/twitter/callback'
+    failureRedirect: FE_URL
   }),
   function(req, res) {
-    res.redirect(' https://twitter-block.herokuapp.com/');
+    res.redirect(test);
   }
 );
 module.exports = router;
