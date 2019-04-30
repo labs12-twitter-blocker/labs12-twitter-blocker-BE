0cda6b058f1c93814129a389fbb93384c9803ed0
Daniel <danielstarling4@gmail.com>
2019-04-30 14:38:46 -0700

    updated index to match master

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,36 +11,4 @@ router.get('/welcome', (req, res) => {
   res.send('You are logged in');
 });
 
-router.get('/', (req, res) => {
-  db('twitter_users')
-    .then(response => {
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: 'The users information could not be retrieved.' });
-    });
-});
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  if (!id) {
-    res
-      .status(404)
-      .json({ error: 'The user with the specified ID does not exist.' });
-    return;
-  }
-  db('twitter_users as t')
-    .where('t.twitter_users_id', id)
-    .then(response => {
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: 'The user information could not be retrieved.' });
-    });
-});
-
 module.exports = router;
