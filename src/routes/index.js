478923c16e30740bf1ca8829ef03e08aae7e2174
Erin Meredith <ekmeredith@att.net>
2019-04-30 13:53:15 -0400

    added listRoute to index; added some GET endpoints and models

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const authRouter = require('./auth');
+const listRouter = require('./lists/listsRouter')
 
 router.use('/auth', authRouter);
+router.use('/lists', listRouter)
 
 router.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,31 +12,5 @@ router.get('/welcome', (req, res) => {
   res.send('You are logged in');
 });
 
-
-router.get('/', (req, res) => {
-  db('twitter_users')
-  .then(response => {
-      res.status(200).json(response)
-  })
-  .catch(err => {
-      res.status(500).json({error: 'The users information could not be retrieved.'})
-  })
-})
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  if(!id){
-      res.status(404).json({error: 'The user with the specified ID does not exist.'})
-      return;
-  }
-  db('twitter_users as t')
-  .where('t.twitter_users_id', id)
-  .then(response => {
-      res.status(200).json(response)
-  })
-  .catch(err => {
-      res.status(500).json({error: 'The user information could not be retrieved.'})
-  })
-})
 
 module.exports = router;
--- src/routes/lists/listsModel.js
+++ src/routes/lists/listsModel.js
@@ -24,5 +24,38 @@ getById: function(listId) {
     .where('uuid', listId).first()
 },
 
+// get all/cool/block lists created by user
+getByUserCreated: function(userId) {
+    return db('lists as l')
+    .join('twitter_users as tu', 'tu.twitter_id', "l.twitter_id")
+    .where('l.twitter_id', userId)
+}, 
 
+// get all /public lists created by user
+getPublicByUserCreated: function(userId) {
+    return db('lists as l')
+    .join('twitter_users as tu', 'tu.twitter_id', "l.twitter_id")
+    .where('l.twitter_id', userId)
+    .where('public', true)
+}, 
+
+// get all /private lists created by user
+getPrivateByUserCreated: function(userId) {
+    return db('lists as l')
+    .join('twitter_users as tu', 'tu.twitter_id', "l.twitter_id")
+    .where('l.twitter_id', userId)
+    .where('public', false)
+}, 
+
+// get /subscribers/:list_id - all USERS who have subscribed to list
+getSubscribers: function(listId) {
+    return db('list_followers as f')
+    .where('list_id', listId)
+},
+
+// get /points list in order of points (upvotes-downvotes)
+getAllByOrder: function() {
+    return db('lists as l')
+    .orderBy('l.upvotes')
+}
 };
