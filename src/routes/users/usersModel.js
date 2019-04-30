4bb6ac667923dca98041570d3e6f76a528d5a32b
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-30 15:03:00 -0700

    Adds a users lists to the DB

--- src/routes/users/usersModel.js
+++ src/routes/users/usersModel.js
@@ -1,10 +1,18 @@
 const db = require('../../data/db.js');
 
 module.exports = {
-  insertMegaUser,
+    findTwitterUserByTwitterId,
+    insertMegaUser,
+    insertMegaUserList,
+    updateMegaUser,
 };
 
 
+function findTwitterUserByTwitterId(twitter_id) {
+    return db('twitter_users')
+      .where({ twitter_id })
+      .first();
+}
 
 function insertMegaUser(user) {
     return db("twitter_users")
@@ -12,4 +20,19 @@ function insertMegaUser(user) {
       .then(ids => {
         return ids;
       });
+}
+
+function insertMegaUserList(list) {
+    return db("lists")
+      .insert(list)
+      .then(ids => {
+        return ids;
+      });
+}
+
+function updateMegaUser(twitter_id, changes) {
+    return db("twitter_users")
+      .where({ twitter_id })
+      .update(changes)
+      .returning('*');
 }
