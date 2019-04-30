1ad694b1933ced37ec099298fed681a8bb69b82d
Erin Meredith <ekmeredith@att.net>
2019-04-30 12:00:04 -0400

    added GET listModels for all,/public,/private/,/:list_id

--- src/routes/lists/listsModel.js
+++ src/routes/lists/listsModel.js
@@ -1,5 +1,28 @@
 const db = require('../../data/db.js');
 
 module.exports = {
-//   get,
+//   get lists/, /cool, /block,
+get: function() {
+    return db('lists')
+},
+
+// get /public
+get: function() {
+    return db('lists')
+    .where('public', true)
+},
+
+// get /private
+get: function() {
+    return db('lists')
+    .where('public', false)
+},
+
+// get /:list_id
+get: function(listId) {
+    return db('lists')
+    .where('uuid', listId)
+},
+
+
 };
