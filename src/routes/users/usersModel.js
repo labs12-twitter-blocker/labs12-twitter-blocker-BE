8632b8d00872056e17c8cd7478c163dcb9afeb54
ddot727 <ddot727@gmail.com>
2019-04-30 13:45:56 -0400

    created basic users model

--- src/routes/users/usersModel.js
+++ src/routes/users/usersModel.js
@@ -1,8 +1,25 @@
 const db = require('../../data/db.js');
 
 module.exports = {
-//   insert,
+  add,
+  find,
+  findBy,
+  findById
 };
 
+function add(user) {
+  return db("app_users").insert(user)
+}
 
+function find() {
+  return db("users").select("id", "username");
+}
+
+function findBy(filter) {
+  return db("app_users").where(filter);
+}
+
+function findById(id) {
+  return db("app_users").where({ id }).first();
+}
 
