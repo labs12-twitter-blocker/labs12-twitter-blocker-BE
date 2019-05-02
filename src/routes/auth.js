a0e8f2dad8612cfbac833ea307e0b140ff635e5e
Daniel <danielstarling4@gmail.com>
2019-05-02 15:40:41 -0700

    fixed formatting and comments

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -19,11 +19,10 @@ passport.use(
     function (token, tokenSecret, profile, callback) {
 
       //search app users table for twitter id
-
       Users.findById(profile.id).then(user => {
         if (!user) {
 
-          //insert user info into app user db
+          //insert new user info into app user db
           let newUser = {
             "twitter_id": profile.id,
             "screen_name": profile.username,
@@ -36,42 +35,33 @@ passport.use(
             "email": null,
             "is_paying": false
           }
-          // console.log(profile.username)
-          // console.log(newUser)
-          // Users.add(newUser)
 
           axios
             .post(`${url}/users`, newUser).then(
 
               // post /users/mega with screen_name
               axios.post(`${url}/users/mega/${profile.username}`))
+
+          // If user exists it will trigger the else function
         } else {
 
           //GET /users/:twitter_id
           axios.get(`${url}/users/${profile.id}`).then(res => {
-            //   //if exists push to modify user
-            //   if (res.data.users.twitter_id) {
-            // if !exist push to new user
-            console.log("TWITTER USER ID", res.data.users.twitter_id)
+
             let modifiedUser = {
               "screen_name": profile.username,
               "token": token,
               "token_secret": tokenSecret,
             }
-            console.log("PROFILE ID", profile.id);
-            console.log("MODIFIED USER", modifiedUser);
             // axios put by twitter id insert modified user
             axios
               .put(`${url}/users/${profile.id}`, modifiedUser).then(
                 axios.post(`${url}/users/mega/${profile.username}`))
-
           }).catch(error => {
             res.status(400).json(error)
           })
         }
-
         return callback(null, profile);
-
       })
     }));
 
