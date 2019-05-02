54b0fde7776f7bea7b8552ca1325c1b33b0e1d3b
Daniel <danielstarling4@gmail.com>
2019-05-02 10:13:20 -0700

    updating oauth callback function

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const router = require('express').Router();
 const passport = require('passport');
 const Strategy = require('passport-twitter').Strategy;
+const Users = require("./users/usersModel");
 
 passport.use(
   new Strategy(
@@ -10,13 +11,24 @@ passport.use(
       consumerKey: process.env.TWITTER_CONSUMER_KEY,
       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
       callbackURL: process.env.CALLBACK_URL
-    },
+    }, //passportSuccessCallback
     function (token, tokenSecret, profile, callback) {
       console.log(token, tokenSecret);
+      console.log(profile)
       return callback(null, profile);
     }
   )
 );
+
+// function passportSuccessCallback(accessToken, accessTokenSecret, profile, done) {
+//   const twitter_id = profile.id;
+//   const screen_name = profile.username;
+//   // console.log(Users.findById(twitter_id))
+//   // console.log(profile)
+//   console.log(accessToken)
+//   console.log(accessTokenSecret)
+//   return [ screen_name ]
+// }
 
 passport.serializeUser(function (user, callback) {
   callback(null, user);
@@ -26,15 +38,47 @@ passport.deserializeUser(function (obj, callback) {
   callback(null, obj);
 });
 
+// passport.serializeUser(function (user, done) {
+//   done(null, JSON.stringify({
+//     id: user.uid,
+//     accessToken: user.access_token
+
+//   }));
+// });
+
+
+// passport.deserializeUser(function (serialized, done) {
+//   var sessionUser = JSON.parse(serialized);
+//   return BtUser.find({
+//     where: {
+//       id: sessionUser.id,
+//       deactivatedAt: null
+//     }, include: [ {
+//       model: TwitterUser
+//     } ]
+//   }).then(function (user) {
+
+//   }).catch(function (err) {
+//     logger.error(err);
+//     // User not found in DB. Leave the user object undefined.
+//     done(null, undefined);
+//     return null;
+//   });
+// });
+
 router.get('/twitter/login', passport.authenticate('twitter'));
 
 router.get(
   '/twitter/callback',
   passport.authenticate('twitter', {
+
     failureRedirect: process.env.FRONT_END_URL
+
   }),
   function (req, res) {
+
     res.redirect(`${process.env.FRONT_END_URL}/test`);
+    // console.log(token)
   }
 );
 
