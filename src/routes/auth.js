beb938e8343ef8e695c689bcb42a263c7e659a64
Daniel <danielstarling4@gmail.com>
2019-05-08 15:35:16 -0700

    cookie testing

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -68,8 +68,8 @@ passport.serializeUser(function (user, callback) {
   callback(null, user);
 });
 
-passport.deserializeUser(function (obj, callback) {
-  callback(null, obj);
+passport.deserializeUser(function (user, callback) {
+  callback(null, user);
 });
 
 
