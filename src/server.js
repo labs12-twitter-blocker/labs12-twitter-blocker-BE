43bcfc41a297e9de4e2855540c95bf055529b244
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-05-10 13:41:27 -0700

    Merge conflict

--- src/server.js
+++ src/server.js
@@ -45,19 +45,7 @@ const corsOptions = {
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   exposedHeaders: [ 'x-auth-token' ],
-
-
-
-
 };
-//CORS middleware
-var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-  next();
-}
 
 //rest API requirements
 server.use(bodyParser.urlencoded({
