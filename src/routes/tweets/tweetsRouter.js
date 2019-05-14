07384bf373aca479bb85ef0ab8fcd072e06fa052
Daniel <danielstarling4@gmail.com>
2019-05-14 09:23:25 -0700

    fixed post tweet endpoint

--- src/routes/tweets/tweetsRouter.js
+++ src/routes/tweets/tweetsRouter.js
@@ -30,14 +30,14 @@ require('dotenv').config();
 router.post('/', (req, res) => {
   const newStatus = req.body.status;
   const newId = req.body.twitter_user_id
-  res.status(200).json({ message: 'tweet on its way, stop it if you can.' });
-  slowTweet = setTimeout(function () {
-    postTweet(newStatus, newId)
-  }, 1000)
+  res.status(200).json({ message: 'Tweet will post in 60 seconds' });
+  // slowTweet = setTimeout(function () {
+  postTweet(newStatus, newId)
+  // }, 1000)
 });
 
-function postTweet(status, id) {
-  Users.findById(id)
+function postTweet(newStatus, newId) {
+  Users.findById(newId)
     .then(newUser => {
       console.log("NEW USER+++++++++++++++++++++++++++++++++", newUser);
       let client = new Twitter({
@@ -46,10 +46,13 @@ function postTweet(status, id) {
         access_token_key: newUser.token,
         access_token_secret: newUser.token_secret,
       })
+      console.log("HERE")
+      console.log("HERE STATUS", newStatus)
       client
-        .post('statuses/update', status)
+        .post('statuses/update', { status: newStatus })
         .catch(function (error) {
         });
+      console.log("AFTER POST STATUS", newStatus)
     })
 }
 
